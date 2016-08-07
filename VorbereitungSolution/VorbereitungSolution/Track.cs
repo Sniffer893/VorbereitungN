bc0ca4f13e472b8522d33c10f99d1b4afd66c752
Marius <marius.feilhauer@gmx.at>
2016-08-07 17:15:08 +0200

    Commit

--- VorbereitungSolution/VorbereitungSolution/Track.cs
+++ VorbereitungSolution/VorbereitungSolution/Track.cs
@@ -8,5 +8,75 @@ namespace VorbereitungSolution
 {
     class Track
     {
+        private string _title;
+        private int _duration;
+        private Artist _writer;
+        private Artist _performer;
+        private int _year;
+
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+
+            set
+            {
+                _title = value;
+            }
+        }
+
+        public int Duration
+        {
+            get
+            {
+                return _duration;
+            }
+
+            set
+            {
+                _duration = value;
+            }
+        }
+
+        internal Artist Writer
+        {
+            get
+            {
+                return _writer;
+            }
+
+            set
+            {
+                _writer = value;
+            }
+        }
+
+        internal Artist Performer
+        {
+            get
+            {
+                return _performer;
+            }
+
+            set
+            {
+                _performer = value;
+            }
+        }
+
+        public int Year
+        {
+            get
+            {
+                return _year;
+            }
+
+            set
+            {
+                _year = value;
+            }
+        }
     }
 }
